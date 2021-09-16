846effa6b7d80a25e134c4e198b9d4c467affb32
laraxpress <mazed@isrt.ac.bd>
2021-09-17 00:05:57 +0600

    Vue JS : Methods and Event Bindings

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ var app = Vue.createApp({
     return{
       msg : 'Welcome to Vue JS',
       info:{
-        name : 'Vue JS Framework',
+        name : 'Laravel Framework',
         url  : 'http://www.laravel.com',
-        code : "<a href='http://www.bbc.co.uk'>BBB News</a>"
-      }
+        code : '<a href="http://www.bbc.co.uk">BBB News</a>'
+      },
+      count : 0
     };
+  },
+  methods:{
+    increament(){
+       this.count++;
+    },
+    decreament(){
+        this.count--;
+    }
   }
 });
 
