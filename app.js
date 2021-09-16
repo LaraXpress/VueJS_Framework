121830e2af1c796528feb894145111eaa428fbe9
laraxpress <mazed@isrt.ac.bd>
2021-09-16 22:48:58 +0600

    Vue JS : Data and Attribute Bindings

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var app = Vue.createApp({
   data(){
     return{
-      msg : 'Welcome to Vue JS',      
+      msg : 'Welcome to Vue JS',
+      info:{
+        name : 'Laravel Framework',
+        url  : 'http://www.laravel.com',
+        code : "<a href='http://www.bbc.co.uk'>BBB News</a>"
+      }
     };
   }
 });
