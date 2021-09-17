621dccbca0c5a1a65e8a52b6e56d3d8154de284c
laraxpress <mazed@isrt.ac.bd>
2021-09-17 23:45:25 +0600

    Vue JS : Data Binding

---
+++ Data Binding/app.js
@@ -0,0 +1,15 @@
+var app = new Vue({
+	el : "#app",
+	data : {
+		name : '',
+		content_A :{
+			title: 'Facebook', url: "https://www.facebook.com", country: 'USA'
+		},
+		content_B :{
+			title: 'Yahoo', url: 'http://www.yahoo.com', country: 'UK'
+		},
+		html : '<a href="https://www.facebook.com">Facebook</a>'
+
+	},
+
+});
